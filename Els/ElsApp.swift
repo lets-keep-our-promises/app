f0491e0fc2cb2b1b03f83834f9421f9add04a27e
Boseok Son <dya_only@naver.com>
2024-09-11 09:10:37 +0900

    a

---
+++ Els/ElsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElsApp.swift
+//  Els
+//
+//  Created by Boseok Son on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ElsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
